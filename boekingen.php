c5b44ac673980729b55385112d215a5ec80c4b54
Jayr0 <122980334+Jayr0@users.noreply.github.com>
2023-06-28 22:14:05 +0200

    Add files via upload

---
+++ boekingen.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/boekingen.css">
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Wix+Madefor+Display&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Boekingen | Emirates Netherlands</title>
+</head>
+<body>
+<header>
+        <div class="logo">
+            <a href="index.php">
+                <img src="img/emirates-logo-badge (1).svg" alt="Logo">
+            </a>
+        </div>
+        <nav>
+            <ul>
+                <li id="header1">
+                    <a href="overons.php">Over ons</a>
+                </li>
+                <li id="header2">
+                    <a href="informatie.php">Informatie</a>
+                </li>
+                <li id="header3">
+                    <a href="contact.php">Contact</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="login-container">
+            <div class="login"><a href="login.php">Inloggen</a></div>
+        </div>
+    </header>
+<main>
+    <h1>Boekingen</h1>
+    <div class="container">
+        <div id="column" class="destination">
+            <h3>Bestemmingen</h3>
+            <ul>
+                <li> 
+                    <p>Amsterdam</p>
+                </li>
+                <li> 
+                    <p>Dubai</p>
+                </li>
+                <li> 
+                    <p>Eindhoven</p>
+                </li>
+            </ul>
+        </div>
+        <div id="column" class="email">
+            <h3>Email</h3>
+            <ul>
+                <li> 
+                    <p>123@email.com</p>
+                </li>
+                <li> 
+                    <p>123@email.com</p>
+                </li>
+                <li> 
+                    <p>123@email.com</p>
+                </li>
+            </ul>
+        </div>
+        <div id="column" class="name">
+            <h3>Naam</h3>
+            <ul>
+                <li> 
+                    <p>Jayro Peters</p>
+                </li>
+                <li> 
+                    <p>Khalil Henane</p>
+                </li>
+                <li> 
+                    <p>Bart Kuppeveld</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</main>
+<footer>
+        <p>Volg ons op onze socials </p>
+        <ul class="socials">
+            <li><a id="instagram" href="#" target="_blank"><i class="fa fa-instagram"></i></a></li>
+            <li><a id="twitter" href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+            <li><a id="facebook" href="#" target="blank"><i class="fa fa-linkedin-square"></i></a></li>
+        </ul>
+        <a id="privacybeleid" href="privacy.php">Privacybeleid & Algemene voorwaarden</a>
+    </footer>
+    <div class="footerbottom">
+        <h1>copyright ©2023 Emirates | All Rights Reserved.</h1>
+    </div>
+</body>
+</html>
